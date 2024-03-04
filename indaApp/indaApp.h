312e44a90461778514079328adb8ede18044e3e9
ErgoTechKG <83173948+ErgoTechKG@users.noreply.github.com>
2024-03-04 12:04:04 +0600

    Initial Commit

---
+++ indaApp/indaApp.h
@@ -0,0 +1,18 @@
+//
+//  indaApp.h
+//  indaApp
+//
+//  Created by Chunyi Liu on 3/4/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for indaApp.
+FOUNDATION_EXPORT double indaAppVersionNumber;
+
+//! Project version string for indaApp.
+FOUNDATION_EXPORT const unsigned char indaAppVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <indaApp/PublicHeader.h>
+
+
